18dcf9f9e5a824534eb0ba3a9ad0bcdd2fa6949f
DavidBao003 <1353406961@qq.com>
2024-09-18 20:17:47 +0800

    finish Q2 Linear

--- hw2/python/needle/nn/nn_basic.py
+++ hw2/python/needle/nn/nn_basic.py
@@ -88,12 +88,15 @@ class Linear(Module):
         self.out_features = out_features
 
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        self.weight = init.kaiming_uniform(in_features, out_features, device=device, dtype=dtype)
+        if bias:
+            self.bias = init.kaiming_uniform(out_features, 1, device=device, dtype=dtype)
+            self.bias = self.bias.transpose()
         ### END YOUR SOLUTION
 
     def forward(self, X: Tensor) -> Tensor:
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        return X.matmul(self.weight) + self.bias
         ### END YOUR SOLUTION
 
 
