1cd596f1769e9e879e684b560074853c57884a28
DavidBao003 <1353406961@qq.com>
2024-09-18 20:30:38 +0800

    finish Q2 LogSumExp

--- hw2/python/needle/ops/ops_logarithmic.py
+++ hw2/python/needle/ops/ops_logarithmic.py
@@ -29,12 +29,25 @@ class LogSumExp(TensorOp):
 
     def compute(self, Z):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        z_origin = array_api.max(Z, axis=self.axes, keepdims=True)
+        z_reduce = array_api.max(Z, axis=self.axes)
+        Z = Z - z_origin
+        return array_api.log(array_api.sum(array_api.exp(Z), axis=self.axes)) + z_reduce
         ### END YOUR SOLUTION
 
     def gradient(self, out_grad, node):
         ### BEGIN YOUR SOLUTION
-        raise NotImplementedError()
+        z = node.inputs[0]
+        max_z = z.realize_cached_data().max(self.axes, keepdims=True)
+        exp_z = exp(z - max_z)
+        sum_exp_z = summation(exp_z, self.axes)
+        grad_sum_exp_z = out_grad / sum_exp_z
+        expand_shape = list(z.shape)
+        axes = range(len(expand_shape)) if self.axes is None else self.axes
+        for axis in axes:
+            expand_shape[axis] = 1
+        grad_exp_z = grad_sum_exp_z.reshape(expand_shape).broadcast_to(z.shape)
+        return grad_exp_z * exp_z
         ### END YOUR SOLUTION
 
 
